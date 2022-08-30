3cc45f67e69755550c21e9af3ebfd2e7db03eba7
Q-T5 <bikathimartin@outlook.com>
2022-08-30 21:02:10 +0300

    Added ability to change themes.

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -18,13 +18,13 @@
   <nav class="shadow-lg h-12 p-2 flex justify-between items-center">
     <div class="pl-5">
       <div class="daisyui-btn-group">
-        <button class="daisyui-btn daisyui-btn-sm lowercase">
+        <button class="daisyui-btn daisyui-btn-sm lowercase no-animation" @click="changeAppTheme">
           <div class="daisyui-tooltip daisyui-tooltip-bottom" data-tip="theme">
-            <font-awesome-icon icon="fa-sun" /> /
+            <font-awesome-icon icon="fa-sun" class="text-yellow-500" /> /
             <font-awesome-icon icon="fa-moon" />
           </div>
         </button>
-        <button class="daisyui-btn daisyui-btn-sm lowercase">
+        <button class="daisyui-btn daisyui-btn-sm lowercase no-animation">
           <div class="daisyui-tooltip daisyui-tooltip-bottom" data-tip="settings">
             <font-awesome-icon icon="fa-gear" />
           </div>
@@ -32,20 +32,37 @@
       </div>
     </div>
     <div class="flex items-center space-x-2">
-      <h1>Your Title Here</h1>
+      <h1 class="text-3xl">Your Title Here</h1>
       <div class="daisyui-tooltip daisyui-tooltip-bottom" data-tip="change title">
         <font-awesome-icon icon="fa-pencil" class="text-sm" />
       </div>
     </div>
     <div class="flex items-center space-x-2">
       <img src="../assets/dog-png.png" alt="dog-logo" class="w-10">
-      <h2>Vue Teir App</h2>
+      <h2 class="text-xl">Vue Teir App</h2>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  "name": "NavbarComp"
+  "name": "NavbarComp",
+  data() {
+    return {
+
+    }
+  },
+  "methods": {
+    "changeAppTheme": function() {
+      const targetElement = document.getElementById("theme-switcher");
+      const attributeValue = targetElement.getAttribute("data-theme");
+
+      if(attributeValue === "dark") {
+        targetElement.setAttribute("data-theme", "garden");
+      } else if(attributeValue === "garden") {
+        targetElement.setAttribute("data-theme", "dark");
+      }
+    }
+  }
 }
 </script>
