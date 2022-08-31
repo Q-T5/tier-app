80bd4a7be3bc65fdf965ed9ffaf81f0959e0302c
Q-T5 <bikathimartin@outlook.com>
2022-08-31 15:47:24 +0300

    Added editable tier names.

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -15,13 +15,37 @@
 -->
 
 <template>
-  <div>
-    main component
+  <div class="flex flex-col justify-start h-full" v-for="tierTitle, index in defaultTierTitles" :key="tierTitle.index">
+    <div class="flex w-full">
+      <div class="bg-red-500 h-20 w-[20%] text-3xl flex flex-col justify-center pl-2">
+        <h1 contenteditable="true" class="outline-none" 
+        @click="titleClicked(index)"
+        @input="changeTierTitle($event, index)">{{ tierTitle }}</h1>
+      </div>
+      <div class="bg-green-500 w-[80%]">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+let currentTierTitle = "";
 export default {
-    "name": "MainComp"
+  "name": "MainComp",
+  data() {
+    return {
+      "defaultTierTitles": ["Tier A", "Tier B", "Tier C", "Tier D", "Tier E"]
+    }
+  },
+  "methods": {
+    "titleClicked": function(index) {
+      currentTierTitle = this.defaultTierTitles[index];
+    },
+    "changeTierTitle": function(event, index) {
+      const usersTierTitle = event.target.innerText;
+      this.defaultTierTitles[index] = usersTierTitle;
+    }
+  }
 }
 </script>
