c6c620a5055b1528db65f9a0f4bf5bd549a688e5
Q-T5 <bikathimartin@outlook.com>
2022-08-31 20:59:46 +0300

    Created the tier system.

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -15,15 +15,25 @@
 -->
 
 <template>
-  <div class="flex flex-col justify-start h-full" v-for="tierTitle, index in defaultTierTitles" :key="tierTitle.index">
-    <div class="flex w-full">
-      <div class="bg-red-500 h-20 w-[20%] text-3xl flex flex-col justify-center pl-2">
-        <h1 contenteditable="true" class="outline-none" 
-        @click="titleClicked(index)"
-        @input="changeTierTitle($event, index)">{{ tierTitle }}</h1>
-      </div>
-      <div class="bg-green-500 w-[80%]">
+  <div class="h-full">
+    <div class="flex odd:bg-secondary/75" v-for="tierTitle, index in defaultTierTitles" :key="tierTitle.index">
+      <div class="flex w-full shadow-primary border-b-2 border-t-2 border-primary">
+        <div class="h-20 w-[20%] text-3xl flex flex-col justify-center pl-2 border-r-2 border-r-primary">
+          <h1 contenteditable="true" class="outline-none" 
+          @click="titleClicked(index)"
+          @input="changeTierTitle($event, index)">{{ tierTitle }}</h1>
+        </div>
+        <div class=" w-[80%]">
 
+        </div>
+      </div>
+    </div>
+    <div class="flex w-full bg-red-300 mt-2 h-fit">
+      <div class="w-[20%] h-20 text-3xl flex flex-col justify-center pl-2 border-r-2 border-r-primary">
+        Items
+      </div>
+      <div class="w-[80%] h-20">
+        Items list
       </div>
     </div>
   </div>
